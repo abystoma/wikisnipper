9b58cb37e8a3e99cdbcc2cf83caa3f2bdf9ead1d
flukehermit <zayed.humayun@g.bracu.ac.bd>
2022-04-16 14:54:08 +0600

    Improved css layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import { useState, useRef } from 'react';
+import { FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div class = "FooterWrapper">
+      <a
+        href={"https://github.com/abystoma/wikisnipper"}
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Github Repo Link"
+      >
+        <FaGithub  size={40} color={`#2a2d2f`} aria-hidden="true" focusable="false" />
+      </a>
+    </div>
+  ); 
+};
 
 const App = () => {
     
@@ -18,21 +34,26 @@ const App = () => {
   const handleInputChange = ({ target }) => setQuery(target.value);
 
   return (
-    <div>
-    <h1>Wikisnipper</h1>
+      
+        <div>
+      <body>
 
-      <form onSubmit={handleSubmit}>
-      <input            
-          value={query}
-          onChange={handleInputChange}
-          placeholder="Type to search..."
-          autoComplete="off"
-          ref={inputFieldRef}
-      />
-      <button type="submit">Search</button>
-    </form> 
-    </div>
-    
+          <h1>Wikisnipper</h1>
+        <div class = "search-area">
+          <form onSubmit={handleSubmit}>
+          <input            
+              value={query}
+              onChange={handleInputChange}
+              placeholder="Type to search..."
+              autoComplete="off"
+              ref={inputFieldRef}
+          />
+          <button type="submit">Search</button>
+          </form> 
+        </div>
+        <Footer/>
+    </body>
+    </div>   
  
   )
 }
